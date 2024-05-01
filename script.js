f21df7df02f482002f690a31cd85286db432cc26
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-01 10:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let btns = document.querySelectorAll(".btn");
+let audio = [];
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let sound = btn.textContent.trim().toLowerCase();
+    play(sound + ".mp3");
+  });
+});
+function play(sound) {
+  let newAudio = new Audio("./sounds/" + sound);
+  newAudio.play();
+  audio.push(newAudio);
+}
+
+document.querySelector(".stop").addEventListener("click", () => {
+  console.log("stop");
+  audio.forEach((sound) => {
+    // console.log(sound);
+    sound.pause();
+    sound.currentTime = 0;
+  });
+});
